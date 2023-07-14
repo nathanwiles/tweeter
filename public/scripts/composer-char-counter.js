5eb983370b840f1d0326e782a95f7751abd8f692
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-13 22:37:14 -0400

    create function to handle character counting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,18 @@
 /* global $ */
 
+$(document).ready(function () {
+  $("#new-tweet-text").on("input", function () {
+    tweetLength = this.value.length;
+    
+    const counter = $(this).closest("form").find(".counter");
+    console.log(counter);
+    
+    counter.text(140 - tweetLength);
 
-$(document).ready(function() {
-
-console.log("ready!");
+    if (tweetLength > 140) {
+      counter.addClass("red");
+    } else {
+      counter.removeClass("red");
+    }
+  });
 });

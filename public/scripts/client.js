86c659ac3d2cc59c33b23d003adc673ee4e8d2ff
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-13 22:47:47 -0400

    add test code.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,7 +18,27 @@ $(document).ready(function () {
   <span>${tweet.created_at}</span>
   </footer> 
   </article>`);
-  };
 
   return $tweet;
+  };
+  
+  
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  created_at: 1461116232227
+};
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like 
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.  
 });

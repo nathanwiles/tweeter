8c3f2f254e388f7185e62aae5b089e16f6e07f2a
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-13 22:40:18 -0400

    setup html template for loading a tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,22 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
+  const createTweetElement = function (tweet) {
+    const $tweet = $(`<article class="tweet">
+  <header class="tweet-header"> 
+  <span>${tweet.user.avatars}</span>
+  <span>${tweet.user.name}</span> 
+  </header>
+  <div class="tweet-body">
+  <p>${tweet.content.text}</p>
+  </div>
+  <footer class="tweet-footer>
+  <span>${tweet.user.handle}</span>
+  <span>${tweet.created_at}</span>
+  </footer> 
+  </article>`);
+  };
 
+  return $tweet;
+});

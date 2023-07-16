ff42795a0b47f4b042dbbd59df96b3c82c14d3f1
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-16 18:17:13 -0400

    prepend tweets to container.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -65,8 +65,11 @@ $(document).ready(function () {
     .then((data) => {
       for (const tweet of data) {
         const $tweet = createTweetElement(tweet);
-        $("#tweets-container").append($tweet);
+        $("#tweets-container").prepend($tweet);
       }
+    })
+    .error((err) => {
+      console.log(err);
     });
   };
 

1e16662c97027a802820509e590cca53c67bdefc
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-16 13:38:17 -0400

    update client to get tweets from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,7 +2,7 @@
  * @file
  * @description contains the JS code for tweeter client. Author: Nathan Wiles.
  * @requires ./helpers/get-time-since.js must be included before this file in html.
- * 
+ *
  */
 
 $(document).ready(function () {
@@ -18,16 +18,19 @@ $(document).ready(function () {
     const timeSince = getTimeSince(tweet.created_at);
     const $tweet = $(`
       <article class="tweet">
+
         <header> 
           <span class="user-info">
-          <img src='${tweet.user.avatars}' alt="${tweet.user.name}'s picture" ></img>
-          <span class="user-name">${tweet.user.name}</span>
+            <img src='${tweet.user.avatars}' alt="${tweet.user.name}'s picture" ></img>
+            <span class="user-name">${tweet.user.name}</span>
           </span> 
           <span class="user-handle">${tweet.user.handle}</span>
         </header>
+
         <div class="text">
           <p>${tweet.content.text}</p>
         </div>
+
         <nav class="footer">
           <div class="tweet-timestamp"> ${timeSince} </div>
           <nav class="tweet-icons">
@@ -36,52 +39,37 @@ $(document).ready(function () {
             <i class="fas fa-heart icon"></i>
           </nav>
         </nav> 
+
       </article>
     `);
-
     return $tweet;
   };
 
   /**
-   * @param {array} tweets
+   * @requires jQuery
+   * @requires createTweetElement()
    * 
-   * @description loops through tweets calls createTweetElement for each tweet.
+   * @description first performs and ajax request to get tweets from server. 
+   * Then loops through tweets calling createTweetElement() for each tweet.
    * @side_effect appends each tweet to the #tweets-container.
-   * 
+   *
    * @returns {void}
    */
 
-  const renderTweets = function (tweets) {
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $("#tweets-container").append($tweet);
-    }
+  const renderTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+    })
+    .then((data) => {
+      for (const tweet of data) {
+        const $tweet = createTweetElement(tweet);
+        $("#tweets-container").append($tweet);
+      }
+    });
   };
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-  renderTweets(data);
+
+  renderTweets();
 });

96f2162b3787b9948836d19ee2b63d7128faffc3
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-16 23:21:06 -0400

    add documentation + error reset functionality

--- public/scripts/helpers/composer-char-counter.js
+++ public/scripts/helpers/composer-char-counter.js
@@ -1,18 +1,30 @@
 /* global $ */
 
 $(document).ready(function () {
+  /**
+   * @event_handler listens for input on the new tweet form.
+   * 
+   * @response changes the text of the counter to the number of characters remaining.
+   * @responce changes the color of the counter to red if the number of characters exceeds 140.
+   * @responce clears error styling if the number of characters meet the requirements. 
+   *
+   */
   $("#new-tweet-text").on("input", function () {
-    tweetLength = this.value.length;
-    
+    const tweetLength = this.value.length;
+    const invalidTextAlert = $(this)
+      .closest("form")
+      .find("#invalid-text-alert");
+    const newTweetText = $(this).closest("form").find("#new-tweet-text");
     const counter = $(this).closest("form").find(".counter");
-    console.log(counter);
-    
     counter.text(140 - tweetLength);
 
     if (tweetLength > 140) {
       counter.addClass("red");
-    } else {
+    } else if (tweetLength > 0){
       counter.removeClass("red");
+      newTweetText.removeClass("red-border-bottom");
+      newTweetText.addClass("dark-border-bottom");
+      invalidTextAlert.hide().slideUp().empty();
     }
   });
 });

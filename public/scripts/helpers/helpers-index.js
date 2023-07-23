2abb07d9faca09e5e5d1c2f307c7ebfb2114d303
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-23 15:39:25 -0400

    lint

--- public/scripts/helpers/helpers-index.js
+++ public/scripts/helpers/helpers-index.js
@@ -1,9 +1,9 @@
 // compile helper functions
-import {createTweetElement} from './create-tweet-element.js';
-import {renderTweets} from './render-tweets.js';
-import {loadTweets} from './load-tweets.js';
-import { flagTweetError } from './flag-tweet-error.js';
-import { validateTweet } from './validate-tweet.js';
+import { createTweetElement } from "./create-tweet-element.js";
+import { renderTweets } from "./render-tweets.js";
+import { loadTweets } from "./load-tweets.js";
+import { flagTweetError } from "./flag-tweet-error.js";
+import { validateTweet } from "./validate-tweet.js";
 
 //export helper functions as an object
 export const helpers = {
@@ -12,5 +12,4 @@ export const helpers = {
   loadTweets,
   renderTweets,
   createTweetElement,
-}
-
+};

8396ce89f890f9b36a4bc6f9347a0e8030e7667c
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-07-23 14:38:37 -0400

    fix bug: no animation

--- public/scripts/event-handlers/return-to-top.js
+++ public/scripts/event-handlers/return-to-top.js
@@ -6,39 +6,35 @@ $(document).ready(function () {
   const $window = $(window);
   const $newTweet = $(".new-tweet");
 
-  
-    /* mobile event */
-    $window.scroll(function() {
-      if ($window.scrollTop() > 100) {
-        $btn.fadeIn();
-      } else {
-        $btn.fadeOut();
-      }
-      
-    })
-    
-    $btn.click(function () {
-      if($window.width() < 1024)
-        $('body, html').animate({ scrollTop: 0}, 'slow');
-        $newTweet.show().slideDown().closest($('textarea').focus());
-
-
-    
-    });
-    
-    /* desktop event */
-    $timeline.scroll(function () {
-      if ($timeline.scrollTop() > 100) {
-        $btn.fadeIn();
-      } else {
-        $btn.fadeOut();
-      }
-    });
+  /* mobile event */
+  $window.scroll(function () {
+    if ($window.scrollTop() > 100) {
+      $btn.fadeIn();
+    } else {
+      $btn.fadeOut();
+    }
+  });
 
   $btn.click(function () {
-    if($window.width() >= 1024)
-    $timeline.animate({ scrollTop: 0 }, 500);
-    $newTweet.show().slideDown().closest($('textarea').focus());
+    if ($window.width() < 1024) {
+      $("body, html").animate({ scrollTop: 0 }, "slow");
+      $newTweet.show().slideDown().closest($("textarea").focus());
+    }
+  });
 
+  /* desktop event */
+  $timeline.scroll(function () {
+    if ($timeline.scrollTop() > 100) {
+      $btn.fadeIn();
+    } else {
+      $btn.fadeOut();
+    }
+  });
+
+  $btn.click(function () {
+    if ($window.width() >= 1024) {
+      $timeline.animate({ scrollTop: 0 }, "slow");
+      $newTweet.show().slideDown().closest($("textarea").focus());
+    }
   });
 });
